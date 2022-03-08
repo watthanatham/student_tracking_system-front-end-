4c1858a066a902981125b063cae993845d427391
Watthanatham Kruram <61160045@go.buu.ac.th>
2022-03-08 11:47:00 +0700

    remove duplicate menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,10 +2,6 @@
   <div class="app-menu min-vh-100">
     <b-img src="../assets/AI.png" width="260%" class="pt-2 pd-2" ></b-img>
       <b-nav vertical>
-      <b-nav-item to="/" class="">หน้าแรก</b-nav-item>
-      </b-nav>
-      <b-nav vertical><b-nav-item to="/courselist">หลักสูตร</b-nav-item>
-      <b-nav-item to="/student">ข้อมูลนิสิต</b-nav-item>
       <!-- <b-nav-item to="/subjectform">Subject Form</b-nav-item> -->
       <!-- <b-nav-item to="/studentform">Student Form</b-nav-item> -->
 
@@ -14,16 +10,14 @@
       <b-nav-item to="/courselist"><b-icon icon="collection"></b-icon> หลักสูตร</b-nav-item>
       <b-nav vertical>
       <b-nav-item to="/student"><b-icon icon="people"></b-icon> ข้อมูลนิสิต</b-nav-item>
-      <!-- <b-nav-item to="/subjectform">Subject Form</b-nav-item>
-      <b-nav-item to="/studentform">Student Form</b-nav-item>
-      <b-nav-item to="/studentresult">Student Result</b-nav-item>
-      <b-nav-item to="/studentresultform">Student Result Form</b-nav-item>
-      <! <b-nav-item to="/studentImport">Student Import</b-nav-item> -->
-      <b-nav-item to="/studyResult">Study results</b-nav-item>
-      <b-nav-item to="/train_hours">สหกิจ</b-nav-item>
-      <!-- <b-nav-item to="/subject_import">Subject Import</b-nav-item> -->
-      <b-nav-item to="/about">ออกจากระบบ</b-nav-item>
-      <b-nav-item to="/subject_import">Subject Import</b-nav-item> -->
+      <b-nav-item to="/subjectform">(Test) Subject Form</b-nav-item>
+      <b-nav-item to="/studentform">(Test) Student Form</b-nav-item>
+      <b-nav-item to="/studentresult">(Test) Student Result</b-nav-item>
+      <b-nav-item to="/studentresultform">(Test) Student Result Form</b-nav-item>
+      <b-nav-item to="/studentImport">(Test) Student Import</b-nav-item>
+      <b-nav-item to="/studyResult">(Test) Study results</b-nav-item>
+      <b-nav-item to="/train_hours">(Test) สหกิจ</b-nav-item>
+      <b-nav-item to="/subject_import">(Test) Subject Import</b-nav-item>
       <b-nav-item to="/about"><b-icon icon="gear"></b-icon> จัดการผู้ใช้งาน</b-nav-item>
       <b-nav-item to="/about"><b-icon icon="box-arrow-left"></b-icon> ออกจากระบบ</b-nav-item>
       </b-nav>
